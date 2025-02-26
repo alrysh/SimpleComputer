08e856eebed4176541b5f95f3077acf1cc3d768b
iv322s27 <iv322s27@csc.sibsutis.ru>
2025-02-26 16:45:48 +0700

    Add pr01

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 all:
 	make -C console
+	# make -C mySimpleComputer
+
+clean:
+	make -C console clean
+	# make -C mySimpleComputer clean
--- console/test.c
+++ console/test.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+ 
+toggleBitsInRange(int num, int start, int end) {
 
-int
-main ()
+    int num_size = num;
+    int length = 0;
+    while (num_size > 0) {
+        num_size >>= 1;
+        length++;
+    }
+
+    printf("size = %d\n", length);
+
+    int mask = 0b0001111;
+    int shift = length - 6 + 1;
+    int newmask = mask << shift;
+
+    int result = newmask ^ num;
+
+    return result; 
+}
+
+int main(void)
 {
-  printf ("Hello world!\n");
-
-  return 0;
+    int num = 0b010101010;
+    int result = toggleBitsInRange(num, 2, 5);
+    printf("Результат: %d (%b)\n", result, result);
+    
+ 
+    return 0;
 }
